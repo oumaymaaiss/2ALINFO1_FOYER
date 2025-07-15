87f14ab478ce50eac490b528cff01fc7facebaeb
oumaima <Oumayma.Aissaoui@esprit.tn>
2025-07-15 10:56:20 +0100

    add FoyerAspect 4SIM2 test

--- src/test/java/tn/esprit/spring/AOP/FoyerAspect4SIM2Test.java
+++ src/test/java/tn/esprit/spring/AOP/FoyerAspect4SIM2Test.java
@@ -1,4 +1,38 @@
 package tn.esprit.spring.AOP;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.Services.Foyer.IFoyerService;
+
+@SpringBootTest
+@Slf4j
 public class FoyerAspect4SIM2Test {
+
+    @Autowired
+    IFoyerService foyerService;
+
+    @Test
+    public void testAspectForAjouterMethod() {
+        // Appelle une méthode commencer par 'ajouter' pour tester le beforeAdvice2
+        foyerService.ajouterFoyerEtAffecterAUniversite(null, 0L);
+
+        // Attendez-vous à voir dans la console :
+        // "ranni d5alt lil méthode ajouterFoyerEtAffecterAUniversite"
+        // "Ranni méthode ajouter"
+        // "ranni 5rajt mil méthode ajouterFoyerEtAffecterAUniversite"
+        // "Method execution time: ... milliseconds."
+    }
+
+    @Test
+    public void testAspectForGeneralMethod() {
+        // Appelle une méthode qui ne commence pas par 'ajouter'
+        foyerService.findAll();
+
+        // Attendez-vous à voir dans la console :
+        // "ranni d5alt lil méthode findAll"
+        // "ranni 5rajt mil méthode findAll"
+        // "Method execution time: ... milliseconds."
+    }
 }
